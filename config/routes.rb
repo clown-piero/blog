bd253e09d9c2c4a6efa18b696d26755ebf62eec6
otareo <leopon0811@icloud.com>
2021-06-26 11:44:15 +0900

    Helloを表示できるようにしました

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :blogcontents
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "hello" => "application#hello"
 end
+

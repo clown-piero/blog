1e24ec292a1b1cfd259c17d5034490432524e340
otareo <leopon0811@icloud.com>
2021-06-28 00:54:11 +0900

    static_pagesを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   resources :blogcontents
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "hello" => "application#hello"
+  
 end
 

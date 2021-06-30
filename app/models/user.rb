efa3bec3140c822f0624ccdd1bdea6a234d6d25c
otareo <leopon0811@icloud.com>
2021-06-30 16:40:58 +0900

    create User model and add password column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,2 @@
 class User < ApplicationRecord
-    validates :name,  presence: true, length: { maximum: 10 }
-  validates :email, presence: true, length: { maximum: 255 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  
+  get 'users/new'
   get 'static_pages/home'
   get 'static_pages/help'
   get "/" => "static_pages#top"
   
-  resources :blogcontents
-  resources :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "hello" => "application#hello"
-  get "users/new" => "users#new"
+  
+  
   
   
 end

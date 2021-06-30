5c779737f6edcfb06d42c72aec96a46e6ac3148e
otareo <leopon0811@icloud.com>
2021-07-01 00:21:17 +0900

    新規ユーザー登録についてのCreateアクションを完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email], password: params[:password] )
+    if @user.save
+      flash[:notice] = "ユーザー登録完了！"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :name,  presence: true, length: { maximum: 10 }
+    validates :email, presence: true, length: { maximum: 255 }
+    validates :password, presence: true, length: { maximum: 20 }
 end
